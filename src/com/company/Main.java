7e5a83b56fb505305b262fddf1b5079eca86b6e5
Joseph Double <josephdouble@Josephs-MacBook-Pro.local>
2019-01-17 15:53:49 -0500

    Changing target number to 1000 and removing unnecessary comments in code.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,15 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int targetNum = 10;
+        int targetNum = 1000;
         int sum = 0;
 
         for (int counter = 1; counter < targetNum; counter++) {
             if(counter % 3 == 0 || counter % 5 == 0) {
                 sum+=counter;
-                //System.out.println("The number " + counter + " is divisible by 3 or 5.");
             } else {
-                //System.out.println("The number " + counter + " is not divisible.");
             }
         }
         System.out.println("The sum is " + sum + ".");
